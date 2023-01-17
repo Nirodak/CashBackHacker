62afa739f6cc3b94a58c5787d0ab0b9bf42ca254
Родак Николай Игоревич <n.rodak@sdk.ru>
2023-01-17 11:07:07 +0300

    refactor1

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,25 @@
 package ru.netology.service;
 
+
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    private int getActual(int amount) {
+        return service.remain(amount);
+    }
 
     @Test
-    public void
+    public void amountUnder1000() {
+
+        int amount = 2500;
+        int expected = 500;
+
+        assertEquals(getActual(amount), expected);
+
+    }
 
 }
